8bd6234ba646112f45b62f14e44f82c1124c8c99
scottcarnold <scottcarnold@gmail.com>
2016-02-11 12:43:14 -0600

    test commit another class

---
+++ ObjectFilterEngine/src/main/java/org/xandercat/ofe/ChangeType.java
@@ -0,0 +1,11 @@
+package org.xandercat.ofe;
+
+/**
+ * Indicator for if a matched object has been added, changed, or removed 
+ * as compared to the prior list of matched objects.
+ * 
+ * @author Scott Arnold
+ */
+public enum ChangeType {
+	ADDED, CHANGED, REMOVED
+}
